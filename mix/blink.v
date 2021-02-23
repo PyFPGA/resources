10e48b216b64e617b31a80dda1925e1abb024c3e
Rodrigo Alejandro Melo <rodrigomelo9@gmail.com>
2021-02-23 13:25:39 -0300

    mix: simplified versions of blink and top to test mixing languages

---
+++ mix/blink.v
@@ -0,0 +1,22 @@
+module Blink (
+    input  wire clk_i,
+    output wire led_o
+);
+
+localparam DIV = 50000;
+
+reg                   led;
+reg [$clog2(DIV)-1:0] cnt = 0;
+
+always @(posedge clk_i) begin
+    if (cnt == DIV-1) begin
+        cnt = 0;
+        led <= ~led;
+    end else begin
+        cnt = cnt + 1;
+    end
+end
+
+assign led_o = led;
+
+endmodule
---
+++ mix/top.v
@@ -0,0 +1,9 @@
+module Top (
+    input  wire clk_i,
+    output wire led_o
+);
+
+
+Blink dut (.clk_i (clk_i), .led_o (led_o));
+
+endmodule
